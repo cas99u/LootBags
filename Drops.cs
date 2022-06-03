184533460dfbd681e597950edaed429cc6da1e84
cas99u <cas99u@gmail.com>
2022-06-03 14:27:54 -0700

    Fixed loot bag drops

--- Drops.cs
+++ Drops.cs
@@ -10,30 +10,97 @@ namespace LootBags
 
         public override void ModifyGlobalLoot(GlobalLoot globalLoot)
         {
-                if (NPC.downedMoonlord)
-                {
-                    globalLoot.Add(ItemDropRule.Common(ModContent.ItemType<Items.LootBag5>(), Amounts.tierFiveChance));
-                }
-                else if (NPC.downedPlantBoss)
-                {
-                    globalLoot.Add(ItemDropRule.Common(ModContent.ItemType<Items.LootBag4>(), Amounts.tierFourChance));
-                }
-                else if (Main.hardMode)
-                {
-                    globalLoot.Add(ItemDropRule.Common(ModContent.ItemType<Items.LootBag3>(), Amounts.tierThreeChance));
-                }
-                else if (NPC.downedBoss3)
-                {
-                    globalLoot.Add(ItemDropRule.Common(ModContent.ItemType<Items.LootBag2>(), Amounts.tierTwoChance));
-                }
-                else
-                {
-                    globalLoot.Add(ItemDropRule.Common(ModContent.ItemType<Items.LootBag1>(), Amounts.tierOneChance));
-                }
+            IItemDropRule tier5Drop = new ItemDropWithConditionRule(ModContent.ItemType<Items.LootBag5>(), Amounts.tierFiveChance, 1 , 1, new NotCheese());
+            IItemDropRule tier4Drop = new ItemDropWithConditionRule(ModContent.ItemType<Items.LootBag4>(), Amounts.tierFourChance, 1, 1, new NotCheese());
+            IItemDropRule tier3Drop = new ItemDropWithConditionRule(ModContent.ItemType<Items.LootBag3>(), Amounts.tierThreeChance, 1, 1, new NotCheese());
+            IItemDropRule tier2Drop = new ItemDropWithConditionRule(ModContent.ItemType<Items.LootBag2>(), Amounts.tierTwoChance, 1, 1, new NotCheese());
+            IItemDropRule tier1Drop = new ItemDropWithConditionRule(ModContent.ItemType<Items.LootBag1>(), Amounts.tierOneChance, 1, 1, new NotCheese());
+
+            LeadingConditionRule tier5Rule = new LeadingConditionRule(new DownedMoonlord());
+            LeadingConditionRule tier4Rule = new LeadingConditionRule(new Conditions.DownedPlantera());
+            LeadingConditionRule tier3Rule = new LeadingConditionRule(new Conditions.IsHardmode());
+            LeadingConditionRule tier2Rule = new LeadingConditionRule(new DownedSkeletron());
             
+            tier5Rule.OnSuccess(tier5Drop);
+            tier5Rule.OnFailedConditions(tier4Rule);
+
+            tier4Rule.OnSuccess(tier4Drop);
+            tier4Rule.OnFailedConditions(tier3Rule);
+
+            tier3Rule.OnSuccess(tier3Drop);
+            tier3Rule.OnFailedConditions(tier2Rule);
+
+            tier2Rule.OnSuccess(tier2Drop);
+            tier2Rule.OnFailedConditions(tier1Drop);
+
+            globalLoot.Add(tier5Rule);
+
+            
+
+
         }
 		
 
 	}
 
+    public class DownedMoonlord : IItemDropRuleCondition
+    {
+        public bool CanDrop(DropAttemptInfo info)
+        {
+
+            return NPC.downedMoonlord;
+
+        }
+
+        public bool CanShowItemDropInUI()
+        {
+            return true;
+        }
+
+        public string GetConditionDescription()
+        {
+            return "Drops after defeating Moon Lord";
+        }
+    }
+
+    public class DownedSkeletron : IItemDropRuleCondition
+    {
+        public bool CanDrop(DropAttemptInfo info)
+        {
+
+            return NPC.downedBoss3;
+
+        }
+
+        public bool CanShowItemDropInUI()
+        {
+            return true;
+        }
+
+        public string GetConditionDescription()
+        {
+            return "Drops after defeating Skeletron";
+        }
+    }
+
+
+    public class NotCheese : IItemDropRuleCondition
+    {
+        public bool CanDrop(DropAttemptInfo info)
+        {
+
+             return !(info.npc.SpawnedFromStatue || info.npc.CountsAsACritter || info.npc.lifeMax <= 5);
+
+        }
+
+        public bool CanShowItemDropInUI()
+        {
+            return true;
+        }
+
+        public string GetConditionDescription()
+        {
+            return "Does not drop from statue or critter";
+        }
+    }
 }
--- Items/LootBag6.cs
+++ Items/LootBag6.cs
@@ -1,85 +1,85 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Terraria;
-using Terraria.ID;
-using Terraria.ModLoader;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using Terraria;
+//using Terraria.ID;
+//using Terraria.ModLoader;
 
-namespace LootBags.Items
-{
-    public class LootBag6 : ModItem
-    {
-        Bag bag = new Bag();
-        private Random rand = new Random();
-        private Loot loot;
-        private String currentTooltip;
-        public LootBag6()
-        {
-            bag.AddItem(ItemID.FragmentSolar, Amounts.common);
-            bag.AddItem(ItemID.FragmentVortex, Amounts.veryRare);
+//namespace LootBags.Items
+//{
+//    public class LootBag6 : ModItem
+//    {
+//        Bag bag = new Bag();
+//        private Random rand = new Random();
+//        private Loot loot;
+//        private String currentTooltip;
+//        public LootBag6()
+//        {
+//            bag.AddItem(ItemID.FragmentSolar, Amounts.common);
+//            bag.AddItem(ItemID.FragmentVortex, Amounts.veryRare);
 
-            currentTooltip = LootBags.myTooltips[0].text;
-        }
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Loot Bag : Tier Test");
-            Tooltip.SetDefault("Right click to open");
-        }
+//            currentTooltip = LootBags.myTooltips[0].text;
+//        }
+//        public override void SetStaticDefaults()
+//        {
+//            DisplayName.SetDefault("Loot Bag : Tier Test");
+//            Tooltip.SetDefault("Right click to open");
+//        }
 
-        public override void SetDefaults()
-        {
-            Item.width = 24;
-            Item.height = 30;
-            Item.rare = ItemRarityID.White;
-            Item.maxStack = 999;
-            Item.value = 0;
-        }
+//        public override void SetDefaults()
+//        {
+//            Item.width = 24;
+//            Item.height = 30;
+//            Item.rare = ItemRarityID.White;
+//            Item.maxStack = 999;
+//            Item.value = 0;
+//        }
 
-        public override bool CanRightClick()
-        {
-            return true;
-        }
+//        public override bool CanRightClick()
+//        {
+//            return true;
+//        }
 
-        public override void AddRecipes()
-        {
+//        public override void AddRecipes()
+//        {
 
-        }
-
-
-        public override void RightClick(Player player)
-        {
-            loot = bag.Roll(player);
-            player.QuickSpawnItem(player.GetSource_OpenItem(loot.item), loot.item, rand.Next(loot.min, loot.max + 1));
+//        }
 
 
-            currentTooltip = getTooltip(LootBags.myTooltips, player);
-        }
+//        public override void RightClick(Player player)
+//        {
+//            loot = bag.Roll(player);
+//            player.QuickSpawnItem(player.GetSource_OpenItem(loot.item), loot.item, rand.Next(loot.min, loot.max + 1));
 
-        public override void ModifyTooltips(List<TooltipLine> tooltips)
-        {
-            tooltips.Add(new TooltipLine(Mod, "FlavorText", currentTooltip));
-        }
 
-        public override bool OnPickup(Player player)
-        {
-            currentTooltip = getTooltip(LootBags.myTooltips, player);
-            return true;
-        }
+//            currentTooltip = getTooltip(LootBags.myTooltips, player);
+//        }
 
-        private String getTooltip(List<CustomTooltip> tooltips, Player player)
-        {
-            bool playerLucky = player.luck >= 0 ? true : false;
-            int ran = Main.rand.Next(0, tooltips.Count);
-            if (tooltips[ran].lucky == playerLucky)
-            {
-                return tooltips[ran].text;
-            }
-            else
-            {
-                return getTooltip(tooltips, player);
-            }
-        }
-    }
+//        public override void ModifyTooltips(List<TooltipLine> tooltips)
+//        {
+//            tooltips.Add(new TooltipLine(Mod, "FlavorText", currentTooltip));
+//        }
 
-}
+//        public override bool OnPickup(Player player)
+//        {
+//            currentTooltip = getTooltip(LootBags.myTooltips, player);
+//            return true;
+//        }
+
+//        private String getTooltip(List<CustomTooltip> tooltips, Player player)
+//        {
+//            bool playerLucky = player.luck >= 0 ? true : false;
+//            int ran = Main.rand.Next(0, tooltips.Count);
+//            if (tooltips[ran].lucky == playerLucky)
+//            {
+//                return tooltips[ran].text;
+//            }
+//            else
+//            {
+//                return getTooltip(tooltips, player);
+//            }
+//        }
+//    }
+
+//}
